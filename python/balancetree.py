ccb4a3325304f7e7f90b30ffc1ae039331d57c35
asiclzscl@gmail.com <asiclzscl@gmail.com>
2017-12-03 15:01:20 -0800

    added

--- python/balancetree.py
+++ python/balancetree.py
@@ -39,39 +39,102 @@ class TreeBST:
         return self.root
     
 
-    def balance(self):
-        pass
+    @staticmethod
+    def balance(node):
+        if node is None:
+            return 0
+        return TreeBST.height(node.left) - TreeBST.height(node.right)
 
-    def insert(self, k):
+    @staticmethod
+    def insert(node, val):
         #BST insert
         if node is None:
             return TreeNode(val)
         if val < node.val:
-            node.left = insertBST(node.left, val)
+            node.left = TreeBST.insert(node.left, val)
         elif val > node.val:
-            node.right = insertBST(node.right, val)
+            node.right = TreeBST.insert(node.right, val)
         else:
             print("duplicated val:", val)
             return node
         #update hight:
+        node.height = 1 + max(TreeBST.height(node.left), TreeBST.height(node.right))
+
+        #rebalance
+        balance = TreeBST.balance(node)
+
+        #left-left
+        if balance>1 and val < node.left.val: 
+            return TreeBST.rightRotate(node)
+        #right-right
+        if balance<-1 and val > node.right.val: 
+            return TreeBST.leftRotate(node)
+        #left-right
+        if balance>1 and val > node.left.val:
+            node.left = TreeBST.leftRotate(node.left)
+            return TreeBST.rightRotate(node)
+        #right-left
+        if balance<-1 and val < node.right.val:
+            node.right = TreeBST.rightRotate(node.right)
+            return TreeBST.leftRotate(node)
+
+        #unchanged
+        return node
             
-        #Rebalance
-        pass
+    @staticmethod
+    def leftRotate(node):
+        x = node.right
+        xl = x.left
+        
+        x.left = node
+        node.right = xl
+
+        node.height = max(TreeBST.height(node.left), TreeBST.height(node.right))
+        x.height = max(TreeBST.height(x.left), TreeBST.height(x.right))
+        return x
+
+
+    @staticmethod
+    def rightRotate(node):
+        x = node.left
+        xr = x.right
+        x.right = node
+        node.left = xr
+        
+        node.height = max(TreeBST.height(node.left), TreeBST.height(node.right))
+        x.height = max(TreeBST.height(x.left), TreeBST.height(x.right))
+
+        return x
+
+
 
     def find(self, val):
         pass
 
-    def inorder(self, node):
+    @staticmethod
+    def inorder(node):
         if node is None:
             return
-        self.inorder(node.left)
-        print("node:", node.val)
-        self.inorder(node.right)
+        TreeBST.inorder(node.left)
+        print("node:", node.val, " height:", node.height)
+        TreeBST.inorder(node.right)
 
     def next(self):
         pass
 
 
 if __name__ == "__main__":
+    print("build bst from a list")
     bst = TreeBST([5,4,3,2,1])
     bst.inorder(bst.root)
+
+    print("build bst with insertion")
+    root = None
+    root = TreeBST.insert(root, 10)
+    root = TreeBST.insert(root, 20)
+    root = TreeBST.insert(root, 30)
+    root = TreeBST.insert(root, 40)
+    root = TreeBST.insert(root, 50)
+    root = TreeBST.insert(root, 25)
+    TreeBST.inorder(root)
+
